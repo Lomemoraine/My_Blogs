546cd2d60c72700e5b354daa50558b28579da716
“Lomemoraine” <lorrainechepkemoi4@gmail.com>
2022-05-15 06:11:45 +0300

    create table users and posts and do first migration

--- app/models.py
+++ app/models.py
@@ -1,12 +1,6 @@
 from . import db
 
-class User(db.Model):
-    __tablename__ = 'users'
-    id = db.Column(db.Integer,primary_key = True)
-    username = db.Column(db.String(255))
 
-    def __repr__(self):
-        return f'User {self.username}'
 class Quote:
     """
     Blueprint class for quotes consumed from API
@@ -14,3 +8,32 @@ class Quote:
     def __init__(self, author, quote):
         self.author = author
         self.quote = quote
+
+class User(db.Model):
+    __tablename__ = 'users'
+    id = db.Column(db.Integer,primary_key = True)
+    first_name = db.Column(db.String(255))
+    last_name = db.Column(db.String(255))
+    username = db.Column(db.String(255),unique=True)
+    email = db.Column(db.String(255),unique=True,index=True)
+    password_hash = db.Column(db.String(255))
+    pass_secure = db.Column(db.String(255), nullable=False)
+    bio = db.Column(db.String(255))
+    profile_pic_path = db.Column(db.String())
+    posts = db.relationship('Post', backref='user', lazy='dynamic')
+
+    def __repr__(self):
+        return f'User {self.username}'
+class Post(db.Model):
+    __tablename__ = "posts"
+
+    id = db.Column(db.Integer, primary_key = True)
+    post_title = db.Column(db.String)
+    post_content = db.Column(db.Text)
+    posted_at = db.Column(db.DateTime)
+    post_by = db.Column(db.String)
+    user_id = db.Column(db.Integer, db.ForeignKey("users.id"))
+    
+    
+    def __repr__(self):
+        return f"Post Title: {self.post_title}"
