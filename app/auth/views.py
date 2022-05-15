71740711af04819704cebb83ae7f37b4f8656ef9
“Lomemoraine” <lorrainechepkemoi4@gmail.com>
2022-05-15 07:14:50 +0300

    finish up on registration and welcome email

--- app/auth/views.py
+++ app/auth/views.py
@@ -3,6 +3,7 @@ from . import auth
 from ..models import User
 from flask_login import login_user,logout_user,login_required
 from .forms import RegistrationForm,LoginForm
+from ..emaill import mail_message
 from .. import db
 
 @auth.route('/login',methods=['GET','POST'])
@@ -27,8 +28,13 @@ def register():
         db.session.add(user)
         db.session.commit()
 
-        mail_message("Welcome to Raine's Pitching App","email/welcome_user",user.email,user=user)
+        mail_message("Welcome to Raine Blogs","email/welcome_user",user.email,user=user)
 
         return redirect(url_for('auth.login'))
         title = "New Account"
     return render_template('auth/register.html',registration_form = form)
+@auth.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    return redirect(url_for("main.index"))
--- config.py
+++ config.py
@@ -1,7 +1,15 @@
+import os
 class Config:
-    
+    SECRET_KEY ='lorraine'
     SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://lorraine:gift1234@localhost/blog'
     SQLALCHEMY_TRACK_MODIFICATIONS=True
+    
+    #  email configurations
+    MAIL_SERVER = 'smtp.googlemail.com'
+    MAIL_PORT = 587
+    MAIL_USE_TLS = True
+    MAIL_USERNAME = os.environ.get("MAIL_USERNAME")
+    MAIL_PASSWORD = os.environ.get("MAIL_PASSWORD")
     pass
     
 class ProdConfig(Config):
