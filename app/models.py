b39e0caf0adeab67297f183561008afe3b827e01
“Lomemoraine” <lorrainechepkemoi4@gmail.com>
2022-05-15 06:15:45 +0300

    authenticate users

--- app/models.py
+++ app/models.py
@@ -1,4 +1,5 @@
 from . import db
+from werkzeug.security import generate_password_hash,check_password_hash
 
 
 class Quote:
@@ -21,6 +22,19 @@ class User(db.Model):
     bio = db.Column(db.String(255))
     profile_pic_path = db.Column(db.String())
     posts = db.relationship('Post', backref='user', lazy='dynamic')
+    
+    
+    @property
+    def password(self):
+        raise AttributeError('You cannot read the password attribute')
+
+    @password.setter
+    def password(self, password):
+        self.pass_secure = generate_password_hash(password)
+
+
+    def verify_password(self,password):
+            return check_password_hash(self.pass_secure,password)
 
     def __repr__(self):
         return f'User {self.username}'
